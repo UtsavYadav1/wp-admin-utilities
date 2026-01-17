6264568eb2bae964301d154503c87aa8b7cdc69a
Utsav Yadav <dsav2613@gmail.com>
2026-01-17 20:41:53 +0530

    Initial commit: Plugin header and security check

---
+++ wp-admin-utilities.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Basic Plugin
+ * Plugin URI:  https://example.com/basic-plugin
+ * Description: A brief description of what the plugin does.
+ * Version:     1.0.0
+ * Author:      Your Name
+ * Author URI:  https://example.com
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: basic-plugin
+ */
+
+// Prevent direct access to this file
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
